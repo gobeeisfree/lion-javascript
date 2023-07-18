0b70a646bb0560d7c25c7f74248fddf8dc235fe6
gobeeisfree <gobeeisfree@gmail.com>
2023-07-18 16:06:08 +0900

    add. 11일차 수업 - css 함수 만들기 - event

--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -1,3 +1,4 @@
+/* ---------------------------------- class --------------------------------- */
 function addClass(node, className) {
   if (typeof node === 'string') node = getNode(node);
   if (typeof className !== 'string') {
@@ -24,4 +25,22 @@ const toggleClass = (node, className) => {
     throw new TypeError('toggleClass 함수의 두 번째 인수는 문자 타입이어야 합니다.');
   };
   return node.classList.toggle(className);
+}
+
+/* ----------------------------------- css ---------------------------------- */
+const setCss = (node, prop, value) => {
+  if (typeof node === 'string') node = getNode(node);
+  if (!(prop in document.body.style)) throw new SyntaxError('setCss 함수의 두번째 인자인 prop은 유효한 css 속성이 아닙니다.');
+  if (!value) throw new SyntaxError('setCss 함수의 세번째 인수는 필수값입니다.')
+  node.style[prop] = value;
+}
+
+const getCss = (node, prop) => {
+  if (typeof node === 'string') node = getNode(node);
+  if (!(prop in document.body.style)) throw new SyntaxError('getCss 함수의 두번째 인자인 prop은 유효한 css 속성이 아닙니다.');
+  return getComputedStyle(node).getPropertyValue(prop);
+}
+
+const css = (node, prop, value) => {
+  return !value ? getCss(node, prop) : setCss(node, prop, value);
 }
