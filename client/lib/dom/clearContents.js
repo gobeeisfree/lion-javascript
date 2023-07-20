4fd001304b3bbfb8ecc422fe88d167e0bb2574a6
gobeeisfree <gobeeisfree@gmail.com>
2023-07-20 14:41:27 +0900

    add. 계산기 만들기 - page1 - page2

---
+++ client/lib/dom/clearContents.js
@@ -0,0 +1,9 @@
+const clearContents = node => {
+  if (typeof node === 'string') node = getNode(node) 
+  if (node.nodeName === 'INPUT' || node.nodeName === 'TEXTAREA') {
+    node.value = ''
+    return;
+  }
+
+  node.textContent = ''
+}
